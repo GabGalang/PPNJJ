64c9022f291516eeb6de5f5a206d13bf6be83768
unknown <galangjohnraphael@gmail.com>
2025-11-27 13:37:55 +0800

    Random Quotes

---
+++ randQuote.py
@@ -0,0 +1,15 @@
+import random
+
+randNumber = random.randint(0, 9)
+quotes = ["“The only thing we have to fear is fear itself.” — Franklin D. Roosevelt”", 
+          "“In the middle of difficulty lies opportunity.” — Albert Einstein",
+          "“Be the change you wish to see in the world.” — commonly attributed to Mahatma Gandhi",
+          "“The journey of a thousand miles begins with a single step.” — Lao Tzu",
+          "“What you think, you become.” — Buddha",
+          "“Success is not final, failure is not fatal: it is the courage to continue that counts.” — Winston Churchill",
+          "“Happiness depends upon ourselves.” — Aristotle",
+          "“Do what you can, with what you have, where you are.” — Theodore Roosevelt",
+          "“If every Porkchops were perfect we wouldn't have hotdogs — Steven Universe",
+          "“To love and be loved is to feel the sun from both sides.” — David Viscott"]
+
+print(quotes[randNumber])
